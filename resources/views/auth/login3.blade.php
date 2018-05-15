7ba7fbf244ecb79d30e5965f979a0ca2e7e819cc
bernazeptian <bernazseptian@gmail.com>
2018-05-15 12:11:24 +0700

    Revisi Login Page 3

--- resources/views/auth/login3.blade.php
+++ resources/views/auth/login3.blade.php
@@ -5,26 +5,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="{{ asset('assets/login_page2/	images/icons/favicon.ico') }}"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/vendor/bootstrap/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/vendor/bootstrap/css/bootstrap.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/vendor/animate/animate.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/vendor/animate/animate.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/vendor/css-hamburgers/hamburgers.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/vendor/css-hamburgers/hamburgers.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/vendor/animsition/css/animsition.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/vendor/animsition/css/animsition.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/vendor/select2/select2.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/vendor/select2/select2.min.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/vendor/daterangepicker/daterangepicker.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/vendor/daterangepicker/daterangepicker.css') }}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/css/util.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('login_page2/css/main.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/css/util.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/login_page2/css/main.css') }}">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -32,7 +32,7 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<div class="login100-form-title" style="background-image: url(images/bg-01.jpg);">
+				<div class="login100-form-title" style="background-image: url('assets/login_page2/images/bg-01.jpg');">
 					<span class="login100-form-title-1">
 						Sign In
 					</span>
@@ -77,21 +77,21 @@
 	</div>
 
 <!--===============================================================================================-->
-	<script src="{{ asset('login_page2/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+	<script src="{{ asset('/assets/login_page2/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('login_page2/vendor/animsition/js/animsition.min.js') }}"></script>
+	<script src="{{ asset('/assets/login_page2/vendor/animsition/js/animsition.min.js') }}"></script>
+<!--=====================/assets/==========================================================================-->
+	<script src="{{ asset('/assets/login_page2/vendor/bootstrap/js/popper.js') }}"></script>
+	<script src="{{ asset('/assets/login_page2/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('login_page2/vendor/bootstrap/js/popper.js') }}"></script>
-	<script src="{{ asset('login_page2/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('/assets/login_page2/vendor/select2/select2.min.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('login_page2/vendor/select2/select2.min.js') }}"></script>
+	<script src="{{ asset('/assets/login_page2/vendor/daterangepicker/moment.min.js') }}"></script>
+	<script src="{{ asset('/assets/login_page2/vendor/daterangepicker/daterangepicker.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('login_page2/vendor/daterangepicker/moment.min.js') }}"></script>
-	<script src="{{ asset('login_page2/vendor/daterangepicker/daterangepicker.js') }}"></script>
+	<script src="{{ asset('/assets/login_page2/vendor/countdowntime/countdowntime.js') }}"></script>
 <!--===============================================================================================-->
-	<script src="{{ asset('login_page2/vendor/countdowntime/countdowntime.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('login_page2/js/main.js') }}"></script>
+	<script src="{{ asset('/assets/login_page2/js/main.js') }}"></script>
 
 </body>
 </html>
